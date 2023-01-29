98cc6cebc0ddd1bcbede2d6c52a795ab39b5a47b
Daniel <danielWong151999@gmail.com>
2023-01-29 14:03:55 +0800

    Add second page and apply function link with second page

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ function validateInputs() {
 
     let getUsername = document.getElementById("username").value
     let getPassword = document.getElementById("password").value
+    let submitButton = document.getElementById("submit_button")
 
     let hasError = false
 
@@ -13,6 +14,7 @@ function validateInputs() {
             document.getElementById("password-error-tip").innerText = "Password should be more than 6 chars"
         } else {
             alert("Welcome!")
+            window.location.href = "second-page.html"
         }
     } catch (error) {
         alert(error)
